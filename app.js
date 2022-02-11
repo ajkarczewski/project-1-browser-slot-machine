f2e7a6e9e94326b777ab6984d2ac8c220d5c41cf
AJKAR <karczewskiamanda@gmail.com>
2022-02-11 13:35:25 -0600

    begin scaffolding

---
+++ app.js
@@ -0,0 +1,44 @@
+/*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+  [1, 1, 1],
+  [2, 2, 2],
+  [3, 3, 3],
+  [4, 4, 4],
+  [5, 5, 5],
+  [6, 6, 6],
+  [7, 7, 7],
+  [8, 8, 8],
+  [9, 9, 9],
+  [10, 10, 10]
+]
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+let wheel1
+let wheel2
+let wheel3
+let winningSpin
+let credit
+let bet
+let winnings
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+resestButton.addEventListener('click', init)
+bet1Button.addEventListener('click',)
+betMaxButton.addEventListener('click',)
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+init()
+
+function init() {
+  wheel1 = [null],
+  wheel2 = [null],
+  wheel3 = [null],
+}
