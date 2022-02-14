7059fb7dfcaee92e8ad9b1a309394f0467614cdc
AJKAR <karczewskiamanda@gmail.com>
2022-02-14 01:09:10 -0600

    add spin function

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
 /*-------------------------------- Constants --------------------------------*/
-const winningCombos = [
-  [1, 1, 1],
-  [2, 2, 2],
-  [3, 3, 3],
-  [4, 4, 4],
-  [5, 5, 5],
-  [6, 6, 6],
-  [7, 7, 7],
-  [8, 8, 8],
-  [9, 9, 9],
-  [10, 10, 10]
-]
-
+// const winningCombos = [
+//   [1, 1, 1],
+//   [2, 2, 2],
+//   [3, 3, 3],
+// ]
 
 
 /*-------------------------------- Variables --------------------------------*/
-let wheel1
-let wheel2
-let wheel3
-let winningSpin
-let credit
-let bet
-let winnings
+let wheel = ["cherry", "grapes", "bananas"]
+// let winningSpin
 
 /*------------------------ Cached Element References ------------------------*/
+// const message = document.querySelector('#message')
+// const resetButton = document.getElementById('reset')
+// const wheel = document.querySelectorAll('wheel')
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-resestButton.addEventListener('click', init)
-bet1Button.addEventListener('click',)
-betMaxButton.addEventListener('click',)
+// resestButton.addEventListener('click', init)
+//spinButton.addEventListener('click', spin)
+// bet1Button.addEventListener('click',)
+// betMaxButton.addEventListener('click',)
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-init()
+// init()
 
-function init() {
-  wheel1 = [null],
-  wheel2 = [null],
-  wheel3 = [null],
-  winningSpin = null
+// function init() {
+//   wheel1 = [null],
+//   wheel2 = [null],
+//   wheel3 = [null],
+//   winningSpin = null
   
-  render()
-}
+//   render()
+// }
+
+// function spin() {
+
+// }
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min)
+// }
+
+// console.log(getRandomInt(1, 4))
+
+let wheel1 = wheel[Math.floor(Math.random() * wheel.length)]
+let wheel2 = wheel[Math.floor(Math.random() * wheel.length)]
+let wheel3 = wheel[Math.floor(Math.random() * wheel.length)]
+
+console.log(wheel1)
+console.log(wheel2)
+console.log(wheel3)
