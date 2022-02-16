fef261369a3e5f1e627cb4ba10a942bf2742ba79
AJKAR <karczewskiamanda@gmail.com>
2022-02-16 00:05:23 -0600

    add pseudocode, alter functions

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ let wheel2
 let wheel3
 let jackPot
 
-// let winningSpin
-
 /*------------------------ Cached Element References ------------------------*/
 const message = document.getElementById('message')
 const resetButton = document.getElementById('reset')
@@ -50,23 +48,53 @@ function render() {
   wheel2El.innerText = `🍇`
   wheel3El.innerText = `🍇`
 
+  jackPot = null
+
 }  
 
 function spin() {
 
-  wheel1Spin = wheel[Math.floor(Math.random() * wheel.length)];
-  wheel2Spin = wheel[Math.floor(Math.random() * wheel.length)];
-  wheel3Spin = wheel[Math.floor(Math.random() * wheel.length)];
+  wheel1 = wheel[Math.floor(Math.random() * wheel.length)];
+  wheel2 = wheel[Math.floor(Math.random() * wheel.length)];
+  wheel3 = wheel[Math.floor(Math.random() * wheel.length)];
 
-  wheel1El.innerText = wheel1Spin
-  wheel2El.innerText = wheel2Spin
-  wheel3El.innerText = wheel3Spin
+  wheel1El.innerText = wheel1
+  wheel2El.innerText = wheel2
+  wheel3El.innerText = wheel3
 
 }
 
-// jackPot = checkJackPot()
 
+jackPot = checkJackPot()
 
-// function checkJackPot() {
-//   if (wheel1Spin === wheel2Spin && wheel2Spin === wheel3Spin)
+function checkJackPot() {
+  if (wheel1 === wheel2 && wheel2 === wheel3) { return true 
+  } else {
+    return false
+  }
+}
+// ^^ tried switching out names to wheelEl or WheelSpin still didn't work
+
+// use winning combos instead?
+
+//include option for null?
+
+console.log(jackPot)
+console.log(jackPot)
+console.log(jackPot)
+console.log(jackPot)
+console.log(wheel1)
+// ^ the deferred DOM Node could not be resolved to a valid node
+// jackPot will ONLY set to true
+
+if (jackPot === true) {
+  message.innerText = `JACKPOT!!!`
+} else if (jackPot === false) {
+  message.innerText = `Sorry, try again`
+}
+  // } else if (jackPot === null) {
+//   message.innerText = `Welcome, take your Spin`
 // }
+
+// init starts game on jackpot and sets message to winning message
+// change function to run checkJackPot only AFTER spin is click
