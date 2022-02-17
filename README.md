cfb12c3475e6871affd8b8856baf45b8a871914a
AJKAR <karczewskiamanda@gmail.com>
2022-02-17 12:11:06 -0600

    add README.md

---
+++ README.md
@@ -0,0 +1,39 @@
+# Slot Machine Simulator 🎰
+
+Welcome to my first General Assembly project! Slot Machine Simulator is an in browser game that simulates a traditional casino slot machine. Go ahead and take your spin (link to game)
+
+## How it started... 📝
+
+-----
+
+picture of wireframe and initial notes
+
+-----
+
+## How it's going 👩‍💻
+
+-----
+
+picture of 
+current game version
+
+-----
+
+## Technologies Used 🛠
+
+----
+
+## Icebox Items 🧊
+
+- I was hoping to include some of my own drawings into the design! While I ended up sketching some of those out, unfortunately because of time constraints they didn't make it into the current version
+- Include animation where coins fall from the screen when a user hits jackpot
+- Implement more of the game features that were in the original wireframe (e.g. x and pay table buttons)
+
+
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-const wheel = ['🍇', '🍒', '🍌']
+const wheel = ['a', 'b', 'c']
 
 
 /*-------------------------------- Variables --------------------------------*/
@@ -31,7 +31,7 @@ init()
 
 function init() {
 
-  jackPot = null
+  //jackPot = null
 
   render()
 }
@@ -39,46 +39,41 @@ function init() {
 function render() {
   message.innerText = `Welcome, take your spin!`
 
-  wheel1El.innerText = `🍇`
-  wheel2El.innerText = `🍇`
-  wheel3El.innerText = `🍇`
+  wheel1El.innerText = 'a'
+  wheel2El.innerText = 'a'
+  wheel3El.innerText = 'a'
 
-  jackPot = null
+  //jackPot = null
 
 }  
 
-function spin(wheel) {
+function spin() {
 
-  wheel1Spin = Math.floor(Math.random() * wheel.length)
-    wheel1El.(wheel[wheel1Spin])
-  // wheel2Spin = Math.floor(Math.random() * wheel.fruits.length)
-  // wheel3Spin = Math.floor(Math.random() * wheel.fruits.length)
+  wheel1 = wheel[Math.floor(Math.random() * wheel.length)];
+  wheel2 = wheel[Math.floor(Math.random() * wheel.length)];
+  wheel3 = wheel[Math.floor(Math.random() * wheel.length)];
 
-  // wheel1El.innerText = wheel1Spin
-  // wheel2El.innerText = wheel2Spin
-  // wheel3El.innerText = wheel3Spin
+  console.log(wheel1, wheel2, wheel3)
+  wheel1El.innerText = wheel1
+  wheel2El.innerText = wheel2
+  wheel3El.innerText = wheel3
 
+  checkJackPot()
 }
 
 
-// jackPot = checkJackPot()
+function checkJackPot() {
+  if (wheel1 === wheel2 && wheel1 === wheel3 && wheel2 === wheel3) {
+    jackPot = true
+  }
+  checkWinner()
+}
 
-// function checkJackPot() {
-//   if (wheel1 === wheel2 && wheel2 === wheel3) { return true 
-//   } else {
-//     return false
-//   } 
-// }
-
-// if (jackPot === true) {
-//   message.innerText = `JACKPOT!!!`
-// } else if (jackPot === false) {
-//   message.innerText = `Sorry, try again`
-// }
-
-// } else if (jackPot === null) {
-//   message.innerText = `Welcome, take your Spin`
-// }
-
-// init starts game on jackpot and sets message to winning message
-// change function to run checkJackPot only AFTER spin is click
+function checkWinner() {
+console.log(jackPot)
+if (jackPot === true) {
+  message.innerText = `JACKPOT!!!`
+} else {
+  message.innerText = `Sorry, try again`
+}
+}
